dc9f849b82503c4a17b79b7f0cb8142f9163188f
Hanyuesummer <Hanyuesummer@csu.fullerton.edu>
2022-10-11 00:51:42 -0700

    converted dataframe to useable json format

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import pandas as pd
+import pandas.io 
+import json
 
 # Create your views here.
 def hello(request):
     if (request.method == 'POST'):
         file = request.FILES['file']
         df = pd.read_csv(file)
+        json_records= df.reset_index().to_json(orient='records')
+        data = []
+        data = json.loads(json_records)
+        print(data)
     else:
         print('This is a get request')
     
