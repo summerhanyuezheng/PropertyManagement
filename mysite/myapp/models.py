0d416c15bab3b8e5f84b800ed2ece09092696d22
Hanyuesummer <Hanyuesummer@csu.fullerton.edu>
2022-10-11 14:37:09 -0700

    created database model

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,12 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Data(models.Model):
+    name = models.CharField(max_length = 200)
+    price = models.IntegerField()
+    rent = models.IntegerField()
+    emi = models.IntegerField()
+    tax = models.IntegerField()
+    exp = models.IntegerField
