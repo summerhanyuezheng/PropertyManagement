2b903cba5d6c46cf5a965fd0b879c39b73b88dcf
Hanyuesummer <Hanyuesummer@csu.fullerton.edu>
2022-10-12 03:52:32 -0700

    created html table for the data, created two new entry in the database for the total monthly expenses and income

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
@@ -10,3 +11,6 @@ class Data(models.Model):
     emi = models.IntegerField()
     tax = models.IntegerField()
     exp = models.IntegerField()
+    #adding a new entry in the database for "expenses_monthly" meaning emi+tax+exp
+    expenses_monthly = models.IntegerField(default=0)
+    income_monthly = models.IntegerField(default=0)
