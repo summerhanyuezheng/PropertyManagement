52151249f8b6096de00812211557c63095fb434f
Hanyuesummer <Hanyuesummer@csu.fullerton.edu>
2022-10-11 23:49:04 -0700

    added delete() function to deal with existing data to avoid repeated data

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -8,6 +8,8 @@ from .models import Data
 # Create your views here.
 def hello(request):
     if (request.method == 'POST'):
+        previous_data = Data.objects.all()
+        previous_data.delete()
         file = request.FILES['file']
         df = pd.read_csv(file)
         json_records= df.reset_index().to_json(orient='records')
@@ -20,6 +22,15 @@ def hello(request):
             emi = d['emi']
             tax = d['tax']
             exp = d['other_exp']
+            #name = name 
+            #left side is the name from models.py Data object
+            #we are taking all the names from models.py
+            #and storing the values into it. the values are: d['property_name']
+            #dt is a Data object
+            dt = Data(name=name, price=price,rent=rent,
+                    emi=emi, tax=tax,exp=exp)
+            #save object dt into the datbase
+            dt.save()
     else:
         print('This is a get request')
     
